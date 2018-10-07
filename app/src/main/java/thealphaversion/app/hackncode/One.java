022b260ef51ab91a6f2caa5204582f32d0ee1f15
thealphaversion <alphabuildv1@gmail.com>
2018-10-07 10:26:37 +0530

    ba

--- app/src/main/java/thealphaversion/app/hackncode/One.java
+++ app/src/main/java/thealphaversion/app/hackncode/One.java
@@ -12,12 +12,12 @@ import io.github.inflationx.calligraphy3.CalligraphyInterceptor;
 import io.github.inflationx.viewpump.ViewPump;
 import io.github.inflationx.viewpump.ViewPumpContextWrapper;
 
-public class PageOne extends AppCompatActivity {
+public class One extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_one);
+        setContentView(R.layout.activity_one);
 
         //Calligraphy
         setCalligraphyFont();
@@ -47,7 +47,7 @@ public class PageOne extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(PageOne.this, Home.class);
+                Intent intent = new Intent(One.this, Home.class);
                 startActivity(intent);
             }
         });
